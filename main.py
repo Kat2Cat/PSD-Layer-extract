9fbc72f29a904f560e6b78501dd92909b65bd6de
Kat2Cat <54067923+Kat2Cat@users.noreply.github.com>
2023-05-17 16:07:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from psd_tools import PSDImage
+from pathlib import Path
+import os
+
+src = input("Folder Location: ")
+dirpath = Path(src)
+
+def extract_layers(psd):
+    Counter = 1
+    for layer in psd:
+        if layer.name == '背景':
+            layer_image = layer.composite()
+            layer_image.save(str(Counter)+'.png')
+            Counter += 1
+
+
+for file in dirpath.glob("**/*"):
+    file_path = os.path.join(src, file)
+    if os.path.isfile(file_path) :
+        psd = PSDImage.open(file_path)
+        extract_layers(psd)
+
